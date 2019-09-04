2d4ccb08f0b39b7527c01870a211345e958a32cc
Janusz G <400@poczta.fm>
2019-09-04 23:12:18 +0200

    Update Player.cs
    reset movement after lose life

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,8 +9,9 @@ public class Player : MonoBehaviour
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
-    private Vector3 moveDirection = Vector3.zero;
+    public Vector3 moveDirection = Vector3.zero;
     private static GameObject playerObject;
+    CharacterController controller;
 
     public static int Lives = 3; // number of lives the player hs
 
@@ -23,12 +24,20 @@ public class Player : MonoBehaviour
         // record the start position of the player
         start_position = transform.position;
         GetPlayerObject();
+        // get the character controller attached to the player game object
+        controller = GetComponent<CharacterController>();
+        
     }
+    
+  
 
     public static void Reset()
     {
         // reset the player position to the start position
+        Physics.SyncTransforms(); // fix https://issuetracker.unity3d.com/issues/charactercontroller-overrides-objects-position-when-teleporting-with-transform-dot-position
         playerObject.transform.position = start_position;
+        playerObject.GetComponent<Player>().moveDirection = new Vector3(0, 0, 0); // reset movement
+
     }
 
     public static GameObject GetPlayerObject()
@@ -40,19 +49,14 @@ public class Player : MonoBehaviour
         return playerObject;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        // get the character controller attached to the player game object
-        CharacterController controller = GetComponent<CharacterController>();
-
 
         // check to see if the player is on the ground
         if (controller.isGrounded)
         {
             // set the movement direction based on user input and the desired speed
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0) * speed;
 
             // check to see if the player should jump
             if (Input.GetButton("Jump"))
@@ -60,9 +64,9 @@ public class Player : MonoBehaviour
         }
 
         // apply gravity to movement direction
-        moveDirection.y -= gravity * Time.deltaTime;
+        moveDirection.y -= gravity * Time.fixedDeltaTime;
 
         // make the call to move the character controller
-        controller.Move(moveDirection * Time.deltaTime);
+        controller.Move(moveDirection * Time.fixedDeltaTime);
     }
 }
