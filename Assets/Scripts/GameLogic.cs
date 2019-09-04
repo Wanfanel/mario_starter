1eb1f1abe75b5826fbe46c10d582b1c310ff0ad4
Janusz G <400@poczta.fm>
2019-09-04 17:22:39 +0200

    Update GameLogic.cs

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -1,6 +1,5 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
+using UnityEngine;
+
 
 public class GameLogic : MonoBehaviour {
 
@@ -8,16 +7,10 @@ public class GameLogic : MonoBehaviour {
 
 	public bool gameOver = false; // is the game over?
 
-	// Use this for initialization
-	void Start () {
-		// find the player component
-		playerComponent = GameObject.FindGameObjectWithTag ("Player").GetComponent<Player> ();
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		// if the player number of lives is zero, game over
-		if (playerComponent.Lives == 0) {
+		if (Player.Lives == 0) {
 			gameOver = true;
 
 			// pause the game
