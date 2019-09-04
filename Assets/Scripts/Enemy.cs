bc8b2e18807523d5feed02d7200c6e3d1279f166
Janusz G <400@poczta.fm>
2019-09-04 23:35:01 +0200

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,90 +1,89 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Enemy : MonoBehaviour {
-
-	// variables taken from CharacterController.Move example script
-	// https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
-	public float speed = 6.0F;
-	public float jumpSpeed = 8.0F;
-	public float gravity = 20.0F;
-	private Vector3 moveDirection = Vector3.zero;
-
-	GameObject playerGameObject; // this is a reference to the player game object
-
-	public Vector3 direction = new Vector3(1.0f, 0.0f, 0.0f); // normalised direction the enemy will move in
-
-	Vector3 start_position; // start position of the enemy
-
-	Vector3 start_direction; // start direction of the enemy
-
-	void Start()
-	{
-		// find the player game object in the scene
-		playerGameObject = GameObject.FindGameObjectWithTag("Player");
-
-		// record the start position
-		start_position = transform.position;
-
-		// record the start direction
-		start_direction = direction;
-	}
-
-	public void Reset()
-	{
-		// reset the enemy position to the start position
-		transform.position = start_position;
-
-		// reset the movement direction
-		direction = start_direction;
-	}
-
-	void Update()
-	{
-		// get the character controller attached to the enemy game object
-		CharacterController controller = GetComponent<CharacterController>();
-
-		// check to see if the enemy is on the ground
-		if (controller.isGrounded) 
-		{
-			// set character controller moveDirection to be the direction I want the enemy to move in
-			moveDirection = direction;
-			moveDirection *= speed;
-		}
+using UnityEngine;
 
 
-		// apply gravity to movement direction
-		moveDirection.y -= gravity * Time.deltaTime;
+public class Enemy : MonoBehaviour
+{
 
-		// make the call to move the character controller
-		controller.Move(moveDirection * Time.deltaTime);
-	}
+    // variables taken from CharacterController.Move example script
+    // https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    public Vector3 direction = Vector3.right; //  direction the enemy will move in
+    Vector3 start_position = Vector3.zero; // start position of the enemy
+    Vector3 start_direction; // start direction of the enemy
 
-	//
-	// This function is called when a CharacterController moves into an object
-	//
-	void OnControllerColliderHit(ControllerColliderHit hit)
-	{
-		// find out what we've hit
-		if (hit.collider.gameObject.CompareTag ("Pipe")) {
-			// we've hit the pipe
+    void Start()
+    {
+        // get the character controller attached to the enemy game object ()
+        controller = GetComponent<CharacterController>();
 
-			// flip the direction of the enemy
-			direction = -direction;
-		} else if (hit.collider.gameObject.CompareTag ("Player")) {
-			// we've hit the player
+        // record the start position
+        start_position = transform.position;
 
-			// get player script component
-			Player playerComponent = playerGameObject.GetComponent<Player> ();
+        // record the start direction
+        start_direction = direction;
+    }
 
-			// remove a life from the player
-			playerComponent.Lives = playerComponent.Lives - 1;
+    public void Reset()
+    {
+        // reset the enemy position to the start position. Cannot transform with no Physics.SyncTransforms()
+        Physics.SyncTransforms();
+        transform.position = start_position;
 
-			// reset the player
-			playerComponent.Reset();
+        // reset the movement direction
+        direction = start_direction;
+    
+    }
 
-			// reset the enemy
-			Reset();
-		}
-	}
+    void FixedUpdate()
+    {
+        // check to see if the enemy is on the ground
+        if (controller.isGrounded)
+        {
+            // set character controller moveDirection to be the direction I want the enemy to move in
+            moveDirection = direction * speed;
+
+        }
+
+
+        // apply gravity to movement direction
+        moveDirection.y -= gravity * Time.fixedDeltaTime;
+
+        // make the call to move the character controller
+        controller.Move(moveDirection * Time.fixedDeltaTime);
+    }
+
+    //
+    // This function is called when a CharacterController moves into an object
+    //
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        // find out what we've hit
+        if (hit.collider.GetComponent<Player>())
+        {
+            // we've hit the player
+
+          
+            // remove a life from the player
+            Player.Lives = Player.Lives - 1;
+            // reset the enemy
+            Reset();
+
+            // reset the player
+            Player.Reset();
+            
+        }
+        else
+        {
+            // We hit objects below us
+            if (hit.moveDirection.y < -0.3f)
+                return;
+
+            // flip the direction of the enemy
+            direction = -direction;
+        }
+    }
 }
