035772762a564c6711d60365e93de4166d775609
Janusz G <400@poczta.fm>
2019-09-05 11:38:02 +0200

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+
+
+public class Enemy : MonoBehaviour
+{
+
+    // variables taken from CharacterController.Move example script
+    // https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    public Vector3 direction = Vector3.right; //  direction the enemy will move in
+    Vector3 start_position = Vector3.zero; // start position of the enemy
+    Vector3 start_direction; // start direction of the enemy
+    
+    void Start()
+    {
+        // get the character controller attached to the enemy game object ()
+        controller = GetComponent<CharacterController>();
+
+        // record the start position
+        start_position = transform.position;
+
+        // record the start direction
+        start_direction = direction;
+    }
+
+    public void Reset()
+    {
+        // reset the enemy position to the start position. Cannot transform with no Physics.SyncTransforms()
+        Physics.SyncTransforms();
+        transform.position = start_position;
+
+        // reset the movement direction
+        direction = start_direction;
+    
+    }
+
+    void FixedUpdate()
+    {
+        // check to see if the enemy is on the ground
+        if (controller.isGrounded)
+        {
+            // set character controller moveDirection to be the direction I want the enemy to move in
+            moveDirection = direction * speed;
+
+        }
+
+
+        // apply gravity to movement direction
+        moveDirection.y -= gravity * Time.fixedDeltaTime;
+
+        // make the call to move the character controller
+        controller.Move(moveDirection * Time.fixedDeltaTime);
+    }
+
+    //
+    // This function is called when a CharacterController moves into an object
+    //
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        // find out what we've hit
+        if (hit.collider.GetComponent<Player>())
+        {
+            // we've hit the player
+            if (hit.moveDirection.y > 0.3f)
+            {
+                hit.collider.GetComponent<Player>().Jump();
+            return;
+            }
+            // remove a life from the player
+            GameLogic.LoseLive();
+            // reset the enemy
+            Reset();
+
+            // reset the player
+            Player.Reset();
+            
+        }
+        else
+        {
+            // We hit objects below us
+            if (hit.moveDirection.y < -0.3f)
+                return;
+
+            // flip the direction of the enemy
+            direction = -direction;
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+
+    // variables taken from CharacterController.Move example script
+    // https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    public Vector3 moveDirection = Vector3.zero;
+    private static GameObject playerObject;
+    CharacterController controller;
+
+    public static int Lives = 3; // number of lives the player hs
+
+
+    static Vector3 start_position; // start position of the player
+
+
+    void Start()
+    {
+        // record the start position of the player
+        start_position = transform.position;
+        GetPlayerObject();
+        // get the character controller attached to the player game object
+        controller = GetComponent<CharacterController>();
+        
+    }
+    
+  
+
+    public static void Reset()
+    {
+        // reset the player position to the start position
+        Physics.SyncTransforms(); // fix https://issuetracker.unity3d.com/issues/charactercontroller-overrides-objects-position-when-teleporting-with-transform-dot-position
+        playerObject.transform.position = start_position;
+        playerObject.GetComponent<Player>().moveDirection = new Vector3(0, 0, 0); // reset movement
+
+    }
+
+    public static GameObject GetPlayerObject()
+    {
+        if (playerObject)
+            return playerObject;
+        else
+            playerObject = GameObject.FindGameObjectWithTag("Player");
+        return playerObject;
+    }
+
+    public void Jump()
+    {
+        moveDirection.y = jumpSpeed;
+    }
+
+    void FixedUpdate()
+    {
+
+        // check to see if the player is on the ground
+        if (controller.isGrounded)
+        {
+            // set the movement direction based on user input and the desired speed
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0) * speed;
+
+            // check to see if the player should jump
+            if (Input.GetButton("Jump"))
+                Jump();
+        }
+
+        // apply gravity to movement direction
+        moveDirection.y -= gravity * Time.fixedDeltaTime;
+
+        // make the call to move the character controller
+        controller.Move(moveDirection * Time.fixedDeltaTime);
+    }
+}
